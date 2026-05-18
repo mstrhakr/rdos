601ec04a92d8c8ff7c80917da6b3700555adbd61
Nick Szittai <nszittai@gmail.com>
2026-05-17 23:07:53 -0400

    Add BuildKit support for improved Docker build performance

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# BuildKit enables parallel layer execution, cache mounts, and better layer caching.
+export DOCKER_BUILDKIT=1
+
 sudo docker build . -t uftc
 sudo rm -f uftc.vhd
 sudo ./d2vm convert uftc:latest -o uftc.vhd --bootloader grub --boot-size 4000 --size 14G --network-manager none "$@"
