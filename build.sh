7c9f2a1c5320a8f92e58829a8ad4fb97c1c167a7
Nick Szittai <nszittai@gmail.com>
2026-05-17 22:51:01 -0400

    Add missing newline at the beginning of build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 sudo docker build . -t uftc
 sudo rm -f uftc.vhd
 sudo ./d2vm convert uftc:latest -o uftc.vhd --bootloader grub --boot-size 4000 --size 14G --network-manager none "$@"
